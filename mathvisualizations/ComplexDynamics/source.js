a20692e08138f4eb4e0d02648fc499ebf76cfe77
Andrew Graven <andrew@graven.com>
2024-08-16 07:13:27 -0700

    Presentation upload, bug fixes

--- mathvisualizations/ComplexDynamics/source.js
+++ mathvisualizations/ComplexDynamics/source.js
@@ -2,6 +2,7 @@ var param_preset_dict = {
   "mandelbrot":     {f:"z^2+c", c:'-.7-.4*i', n:'100', escape:"abs(z)>2", zoom: .75, center: [-.75,0]},
   "tricorn":        {f:"conjugate(z^2)+c",  c:'.1091+i*.502',    n:'50', escape:"abs(z)>2", zoom: .55, center: [-.25,0]},
   "burning ship":   {f:"(abs(re(z))+i*abs(im(z)))^2-c", c:'1.6185+i*0.0471', n:'100', escape:"abs(z)>2", center:[1.7,.05], zoom: 10},
+  "butterfly":      {f:"conjugate(z^2)+c*re(1/z)", c:'.252+i*0', n:'150', escape:"abs(z)>4", center:[0,0], zoom:.7},
   "exponential map":{f:"c*e^(z-1)", c:'1+0*i', n:'25', escape:'re(log(c)+z)>3000', center:[3.5,0], zoom: .08},
   "exp Schwarz":    {f:"c0 = c^2/z;c1 = lambertw(-c0);conjugate(c0/exp(c1+c^2/c1));", c:'1+0*i', n:'50', escape:"abs(lambertw(-c^2/f(z,c)))>abs(c)",center: [1,0], zoom: .5}
 };
@@ -9,6 +10,7 @@ var dyn_preset_dict = {
   "mandelbrot":     {f:"z^2+c", c:'.2541-.0333*i', n:'100', escape:"abs(z)>2", zoom: .65, center: [0,0]},
   "tricorn":        {f:"conjugate(z^2)+c",  c:'.2541-i*0.2302',    n:'50', escape:"abs(z)>2", zoom: .65, center: [0,0]},
   "burning ship":   {f:"(abs(re(z))+i*abs(im(z)))^2-c", c:'-.8217+i*0.1233', n:'100', escape:"abs(z)>2", center:[0,0], zoom: .5},
+  "butterfly":      {f:"conjugate(z^2)+c*re(1/z)", c:'-.4547-i*.7733', n:'150', escape:"abs(z)>4", center:[0,0], zoom:.9},
   "exponential map":{f:"c*e^(z-1)", c:'1.418-i*.119', n:'25', escape:'re(log(c)+z)>3000', zoom:.13, center:[8,0]},
   "exp Schwarz":    {f:"c0 = c^2/z;c1 = lambertw(-c0);conjugate(c0/exp(c1+c^2/c1));", c:'2.92-.48*i', n:'50', escape:"if(re(z)<-5,true,if(abs(lambertw(-c^2/f(z,c)))>abs(c),true,false));", center:[6,0], zoom:.15}
 };
@@ -144,10 +146,10 @@ class FractalPlot {
     if ("move" in this._callbacks) {
       this._movescript += 'javascript("' + this._callbacks.move.join("; ") +';");';
     }
-    
-    this._keydownscript = 'keyval = key();' +
-          'if(keyval=="\'",keyval="l");' +
-          'javascript("' + this._varName + '.keypress(\'"+keyval+"\')");';
+    this._keydownscript = 'javascript("' + this._varName + '.keypress(\'"+"\\" + key()+"\'.charCodeAt(0))");';
+    if ("keydown" in this._callbacks) {
+      this._keydownscript += 'javascript("' + this._callbacks.keydown.join("; ") +';");';
+    }
     this._cindy = CindyJS({
       canvasname: this._canvasName,
       scripts: {
@@ -190,23 +192,18 @@ class FractalPlot {
     this.center = [this._center[0] + vec[0],this._center[1] + vec[1]];
   }
 
-  UpdateRange() {
-    var str = 'zoom='+this._zoom+';'+
-              'center_1='+this._center[0]+';'+
-              'center_2='+this._center[1]+';';
-    this._cindy.evokeCS(str);
-  }
 
   keypress(key) {
     switch(key) {
-      case "»":  this.zoomIn(2); break;
-      case "½":  this.zoomIn(1/2); break;
-      case "&":  this.shift([0,1/(this._zoom*4)]); break;
-      case "(":  this.shift([0,-1/(this._zoom*4)]); break;
-      case "l":  this.shift([1/(this._zoom*4),0]); break;
-      case "%":  this.shift([-1/(this._zoom*4),0]); break;
+      case 187:  this.zoomIn(2); break;
+      case 189:  this.zoomIn(1/2); break;
+      case 38:   this.shift([0,1/(this._zoom*4)]); break;
+      case 40:   this.shift([0,-1/(this._zoom*4)]); break;
+      case 39:   this.shift([1/(this._zoom*4),0]); break;
+      case 37:   this.shift([-1/(this._zoom*4),0]); break;
     }
   }
+
 
   set z0(z0Val) {
     this._z0 = z0Val;
@@ -245,6 +242,7 @@ class FractalPlot {
     this._cindy.evokeCS(str);
   }
 
+
   get z0() {
     this._cindy.evokeCS('javascript("' + this._varName + '._z0 = ' + this._varName + '.CanvToPlot("+Z0.xy+")");');
     return this._z0;
@@ -252,6 +250,10 @@ class FractalPlot {
 
   get zoom() {
     return this._zoom;
+  }
+
+  get center() {
+    return this._center;
   }
 
   get c() {
@@ -350,6 +352,46 @@ var getDZoomInput = function() {
   return parseFloat(document.getElementById("inpdynzoom").value);
 }
 
+//////////
+
+var setCInput = function(cval) {
+  document.getElementById("inpc").value = cval;
+}
+
+var setNInput = function(nval) {
+  document.getElementById("inpn").value = nval;
+}
+
+var setFInput = function(fval) {
+  document.getElementById("inpf").value = fval;
+}
+
+var setDEscInput = function(escval) {
+  document.getElementById("inpje").value = escval;
+}
+
+var setPEscInput = function(escval) {
+  document.getElementById("inpme").value = escval;
+}
+
+var setPCenterInput = function(centerval) {
+  centerval = centerval.map(vals => vals.toPrecision(6)).map(parseFloat);
+  document.getElementById("inpparamcenter").value = centerval;
+}
+
+var setPZoomInput = function(zoomval) {
+  document.getElementById("inpparamzoom").value = zoomval;
+}
+
+var setDCenterInput = function(centerval) {
+  centerval = centerval.map(vals => vals.toPrecision(6)).map(parseFloat);
+  document.getElementById("inpdyncenter").value = centerval;
+}
+
+var setDZoomInput = function(zoomval) {
+  document.getElementById("inpdynzoom").value = zoomval;
+}
+
 
 var apply_changes = function() {
   julia_fract.ApplyPreset(    getPresetDicts()[1]);
@@ -363,15 +405,15 @@ var getPresetDicts = function() {
 
 
 var apply_preset = function(preset_val) {
-  document.getElementById("inpparamcenter").value = param_preset_dict[preset_val].center;
-  document.getElementById("inpparamzoom").value = param_preset_dict[preset_val].zoom;
-  document.getElementById("inpdyncenter").value = dyn_preset_dict[preset_val].center;
-  document.getElementById("inpdynzoom").value = dyn_preset_dict[preset_val].zoom;
-  document.getElementById("inpc").value = param_preset_dict[preset_val].c;
-  document.getElementById("inpn").value = param_preset_dict[preset_val].n;
-  document.getElementById("inpf").value = param_preset_dict[preset_val].f;
-  document.getElementById("inpme").value = param_preset_dict[preset_val].escape;
-  document.getElementById("inpje").value = dyn_preset_dict[preset_val].escape;
+  setPCenterInput(param_preset_dict[preset_val].center);
+  setPZoomInput(param_preset_dict[preset_val].zoom);
+  setDCenterInput(dyn_preset_dict[preset_val].center);
+  setDZoomInput(dyn_preset_dict[preset_val].zoom);
+  setCInput(param_preset_dict[preset_val].c);
+  setNInput(param_preset_dict[preset_val].n);
+  setFInput(param_preset_dict[preset_val].f);
+  setPEscInput(param_preset_dict[preset_val].escape);
+  setDEscInput(dyn_preset_dict[preset_val].escape);
   julia_fract.ApplyPreset(    dyn_preset_dict[preset_val]);
   parameter_fract.ApplyPreset(param_preset_dict[preset_val]);
 }
